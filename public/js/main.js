3ef0073eee477e8eca67a3bc50423e79589a613f
ralfgraef <graef.ralf@gmail.com>
2018-08-07 21:35:17 +0200

    change sticky footer rules

--- public/js/main.js
+++ public/js/main.js
@@ -38,11 +38,4 @@ $(document).ready(function() {
     });
 
   });
-
-  var bodyHeight = $("body").height();
-  var vwptHeight = $(window).height();
-  if (vwptHeight > bodyHeight) {
-  $("footer").css("position","absolute").css("bottom",0);
-  }
-  
 });
