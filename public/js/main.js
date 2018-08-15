d8b091aa92a9941841c762744560eaa0a630be79
ralfgraef <graef.ralf@gmail.com>
2018-08-16 00:23:05 +0200

    add sweetalert

--- public/js/main.js
+++ public/js/main.js
@@ -4,28 +4,56 @@ $(document).ready(function() {
     $('.alert').fadeOut(5000);
   }
   $('.delete-article').on('click', function(e){
-    if(confirm('Bist Du sicher?')) {
       $target = $(e.target);
       console.log('$target: ', $target);
       const id = $target.attr('data-id');
-      $.ajax({
-        type: 'DELETE',
-        url:'/articles/' + id,
-        success: function(response) {
-          window.location.href='/'
-        },
-        error: function(err) {
-          console.log(err);
-        }
+      swal({
+        title: "Bist Du sicher?",
+        text: "Du möchtest die ganze Liste löschen?",
+        icon: "info", /* type: "info", */
+        buttons: [
+          "Nein", 
+          "Ja, absolut!" 
+        ],
+        focusConfirm: false,
+        showCloseButton: true
+      })
+      .then((willDelete) => {
+        if(willDelete){
+          $.ajax({
+            type: 'DELETE',
+            url:'/articles/' + id,
+            success: function(response) {
+              window.location.href='/'
+            },
+            error: function(err) {
+              console.log(err);
+            }
+          }); 
+        } else {
+          return false;
+        };
       });
-    }; 
   });
 
   $('.delete-item').on('click', function(e){
     $target = $(e.target);
     const id = $target.attr('data-id');
     const did = $target.attr('id');
-    $.ajax({
+    swal({
+      title: "Bist Du sicher?",
+      text: "Du möchtest diesen Eintrag löschen?",
+      icon: "info", /* type: "info", */
+      buttons: [
+        "Nein", 
+        "Ja, absolut!" 
+      ],
+      focusConfirm: false,
+      showCloseButton: true
+    })
+    .then((willDelete) => {
+      if(willDelete){
+      $.ajax({
       type: 'PUT',
       url:'/articles/delete_item/' + id + '/' + did,
       success: function(response) {
@@ -35,7 +63,13 @@ $(document).ready(function() {
       error: function(err) {
         console.log(err); 
       }
+      })
+    } else {
+      return false;
+      };
     });
+
+    
   });
 
   $('.checkbox').on('click', function(e) {
